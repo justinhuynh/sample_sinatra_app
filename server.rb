1c635869366b76261994e2a5457109a44d9dd6df
justinhuynh <jhuynh@gmail.com>
2016-02-29 15:18:01 -0500

    initial commit

---
+++ server.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require 'sinatra'
+
+get '/' do
+  "blah"
+end
+
+get '/index' do
+  @something = ["this", "that", "and", "the other"]
+  erb :index
+end
+
+get '/fridge/:that_thing_in_my_fridge' do
+  @some_var = params[:that_thing_in_my_fridge]
+  erb :bobberooo
+end
